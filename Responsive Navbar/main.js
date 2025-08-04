71f4ae4d5c600e3180f19f8e033994542fa746de
SaqiiCoditz <saqiicoditz@gmail.com>
2025-08-05 01:37:19 +0500

    Responsive Navbar

---
+++ Responsive Navbar/main.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("hamburger");
+const links = document.getElementById("links");
+
+btn.onclick = function(){
+    btn.classList.toggle("active");
+    links.classList.toggle("active");
+};
